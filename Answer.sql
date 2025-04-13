d57e133dcdc223de1fbdba4d242fdb7d28fff586
Mishael Matende <113938133+MishMatende@users.noreply.github.com>
2025-04-13 21:36:21 +0300

    Update Answer.sql

--- Answer.sql
+++ Answer.sql
@@ -85,6 +85,7 @@
     -> author_id INT NOT NULL,
     -> PRIMARY KEY (book_id, author_id),
     -> FOREIGN KEY (book_id) REFERENCES book(book_id),
+    -> FOREIGN KEY (author_id) REFERENCES author(author_id),
     -> );
 
      -- Create table shipping method
